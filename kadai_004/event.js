2d1c3303c88fa0ba29e723a64f8a078be7c7db1a
kentayanoki <yanoki111122@gmail.com>
2024-02-04 22:25:11 +0900

    課題4を実施

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+$(window).on('load scroll', (e) => {
+    // loadイベントが発生した場合
+    if(e.type === 'scroll'){
+      console.log('loadイベントが発生しました');
+    }
+    //scrollイベントが場合実施された
+    if(e.type === 'scroll'){
+      console.log('scrollイベントが発生しました');
+    }
+  });
